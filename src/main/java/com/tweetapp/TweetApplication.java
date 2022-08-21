4101321d9ecd669b3d80d43aee2e24b265138dcf
navneetnegi <testingfornsn@gmail.com>
2022-08-21 14:58:17 +0530

    Cors Fixes

--- src/main/java/com/tweetapp/TweetApplication.java
+++ src/main/java/com/tweetapp/TweetApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class TweetApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TweetApplication.class, args);
